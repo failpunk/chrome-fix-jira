466d9271f45d051a97d2f06fc10aac32bedf18d1
Justin Vencel <justin@studymode.com>
2016-04-30 16:21:00 -0700

    first version

---
+++ jira.js
@@ -0,0 +1,15 @@
+if(document.getElementById('view-subtasks')){
+    document.getElementById('viewissuesidebar').appendChild(document.getElementById('view-subtasks'));
+}
+
+if(document.getElementById('linkingmodule')){
+    document.getElementById('viewissuesidebar').appendChild(document.getElementById('linkingmodule'));
+}
+
+if(document.getElementById('activitymodule')){
+    document.getElementById('viewissuesidebar').appendChild(document.getElementById('activitymodule'));
+}
+
+if(document.getElementById('addcomment')){
+    document.getElementById('viewissuesidebar').appendChild(document.getElementById('addcomment'));
+}
